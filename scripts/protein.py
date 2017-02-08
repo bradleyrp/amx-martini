9d75f59f4ef9fd01c185f671959eb7f9bfe89aa3
Ryan Bradley <bradleyrp@gmail.com>
2017-02-08 16:01:39 -0500

    major development

--- scripts/protein.py
+++ scripts/protein.py
@@ -8,5 +8,7 @@ from amx import *
 
 init()
 make_step(settings.step)
+if not os.path.isfile(state.start_structure): 
+	raise Exception('cannot locate state.start_structure: %s'%state.start_structure)
 shutil.copy(state.start_structure,state.here+'protein-start.pdb')
 martinize()
